046f911a65cedd9d7b09780576e25be640707449
Hasnine670817 <hasninetorun@gmail.com>
2025-10-15 13:42:06 +0600

    problem-14 done

---
+++ problem-14.js
@@ -0,0 +1,12 @@
+/*
+🧩 Problem 14: Reverse an Array
+Write a function that reverses the elements of an array.
+Sample Input: [1, 2, 3, 4]
+Sample Output: [4, 3, 2, 1]
+*/
+
+function reverseArray(array) {
+    return array.reverse();
+}
+
+console.log(reverseArray([1, 2, 3, 4])); // [ 4, 3, 2, 1 ]
